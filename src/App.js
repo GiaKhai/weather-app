22fa2404a222cecb960980586c4297a7bcf7925b
GiaKhai <khaigia11111@gmail.com>
2020-12-24 15:07:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import React from "react";
+import { getWeatherData } from './data/weatherapi';
+import { ScaleLoader } from 'react-spinners';
 
 function App() {
+  const [weatherdata, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Hanoi");
+  const [loading, setLoading] = useState(false);
+  
+  const getData = async () => {
+    try {
+      setLoading(true)
+      const data = await getWeatherData(city);
+      setWeatherData(data);
+      console.log(data);
+      setLoading(false);
+    } catch (error) {
+      console.log("False", error);
+      setLoading(false);
+    }
+  }
+    
+    useEffect(() => {
+      getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+  
+  const override = `
+  display : block;
+  margin: 0 auto;
+  border-color: red;
+  `;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="title">
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/861/861059.svg"
+            alt="iconWeather"
+          />
+          <br />
+          <i>WEATHER</i>
+        </div>
+        <div className="search-form">
+          <input
+            type="text"
+            value = {city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter your city name..."
+          ></input>
+          <button type="button" onClick={() => getData()}>
+            Search
+          </button>
+        </div>
+        {loading ? (
+          <div className="loader-container">
+            <ScaleLoader
+              css={override}
+              size={200}
+              color={"#fff"}
+              loading={loading}
+            />
+          </div>
+        ) : (
+          <>
+            {weatherdata !== null ? (
+              <div className="main-container">
+                <h4>Live Weather</h4>
+                <div className="weatherIcon">
+                  <img
+                    src={`http://openweathermap.org/img/w/${weatherdata.data.weather[0].icon}.png`}
+                    alt="imgIcon"
+                  />
+                </div>
+                <h4>{weatherdata.data.weather[0].main}</h4>
+                <div className="temperature">
+                  <h2>{weatherdata.data.main.temp}&deg;C</h2>
+                </div>
+                <div className="location">
+                  <h3>
+                    {" "}
+                    {weatherdata.data.name} - {weatherdata.data.sys.country}{" "}
+                  </h3>
+                </div>
+                <div className="temperature-range">
+                  <h5>
+                    {weatherdata.data.main.temp_max}&deg;C -{" "}
+                    {weatherdata.data.main.temp_min}&deg;C{" "}
+                  </h5>
+                  <h5>Humidity: {weatherdata.data.main.humidity}%</h5>
+                </div>
+              </div>
+            ) : null}
+          </>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/data/weatherapi.js
@@ -0,0 +1,15 @@
+import  axios  from "axios";
+
+const baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "dcc110cee99490ceef7230a5867a319e";
+
+export const getWeatherData = async (cityname) => {
+    try {
+        const data = await axios.get(
+          baseUrl + `q=${cityname}&appid=${apiKey}` 
+        );
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
